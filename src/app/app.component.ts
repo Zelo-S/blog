2be3e5b355613641d14f01e09bfb3d8566171f88
Zelo-S <45673317+Zelo-S@users.noreply.github.com>
2021-07-09 19:22:07 -0400

    Angular Stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+	title = 'blog';
+
+	color = "orange";
+	updateColor() {
+		this.color = "skyblue";
+	}
+
+	userData: any = {};
+	getData(data: NgForm) {
+		console.warn(data);
+		this.userData = data;
+	}
+
+	display = true;
+	toggle() {
+		this.display = !this.display;
+	}
+
+	list: any[] = [];
+	addTask(item: string) {
+		this.list.push({ id: this.list.length, name: item });
+		console.warn(this.list);
+	}
+
+	removeTask(id: number) {
+		this.list = this.list.filter(item => item.id !== id);
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CountryComponent } from './country/country.component';
+
+import { UserAutModule } from './user-aut/user-aut.module';
+import { HeadsComponent } from './heads/heads.component';
+import { HeaderComponent } from './header/header.component';
+
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ProfileComponent,
+    UserListComponent,
+    UserAuthComponent,
+    StudentListComponent,
+    CountryComponent,
+    HeadsComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAutModule,
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
